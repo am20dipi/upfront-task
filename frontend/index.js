e01e919c3eb263e83587f960f36a45cfd01e13bf
Adriana <am20dipi@alum.siena.edu>
2021-06-07 16:48:18 -0400

    Creating notes.md file, adding event listeners for DOMContent Loaded and for click events for buttons, writing functions to handle events

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,4 @@
-// getter functions to call on id's from HTML
-// setting const variables 
+// Declaring Getter Functions
 // calling on the document object to retrieve whatever element was classified under the specified id
 
 const buttonsDiv = () => document.getElementById("buttons")
@@ -8,3 +7,20 @@ const listDiv = () => document.getElementById("list")
 const ulTasksList = () => document.getElementById("tasks-lists")
 
 
+// Events
+
+//DOMContentLoaded => when the DOM finishes loading, listen for the specified events 
+
+document.addEventListener("DOMContentLoaded", () => {
+    // anonymous function
+    showTasksButton.addEventListener("click", handleClick)
+    // handleClick is a callback function so we do not invoke the function; we will use it later! 
+})
+
+const handleClick = () => {
+    // Arrow function => 
+    // Fetch requests return Promises
+    fetch('http://localhost:3000/tasks')
+    .then()
+    .catch(error => console.log("Error!!!"))
+}
