76cacceb41551a30e3557fbee103df72c96395a0
Adriana <am20dipi@alum.siena.edu>
2021-06-07 15:38:36 -0400

    Creating db and migrating first migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create!(name: "Go to carwash", done_by: "", completed: true)
+Task.create!(name: "Pick up grandma", done_by: "", completed: true)
+Task.create!(name: "Go to bank" done_by: "Tuesday", completed: false)
