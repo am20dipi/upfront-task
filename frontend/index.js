3df5e29145eb8ce4a6fb0fd46dd54a40442a48f4
Adriana <am20dipi@alum.siena.edu>
2021-06-07 16:41:04 -0400

    Creating getter functions in index.js to retrieve elements from index.html by id name

---
+++ frontend/index.js
@@ -0,0 +1,10 @@
+// getter functions to call on id's from HTML
+// setting const variables 
+// calling on the document object to retrieve whatever element was classified under the specified id
+
+const buttonsDiv = () => document.getElementById("buttons")
+const showTasksButton = () => document.getElementById("show-tasks")
+const listDiv = () => document.getElementById("list")
+const ulTasksList = () => document.getElementById("tasks-lists")
+
+
