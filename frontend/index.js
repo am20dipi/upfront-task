b4dfad357ef4b6e9d1a8aea597929eb4e1cdfc12
Adriana <am20dipi@alum.siena.edu>
2021-06-07 17:06:12 -0400

    Calling .then() instance methods on fetch to parse response, creating function renderTasks to iterate over each element of the response array and paste it to the page

--- frontend/index.js
+++ frontend/index.js
@@ -13,14 +13,31 @@ const ulTasksList = () => document.getElementById("tasks-lists")
 
 document.addEventListener("DOMContentLoaded", () => {
     // anonymous function
-    showTasksButton.addEventListener("click", handleClick)
+    showTasksButton().addEventListener("click", handleClick)
     // handleClick is a callback function so we do not invoke the function; we will use it later! 
+
 })
 
 const handleClick = () => {
     // Arrow function => 
     // Fetch requests return Promises
     fetch('http://localhost:3000/tasks')
-    .then()
+    .then(response => response.json())
+    // parse the response body into JSON friendly format => array
+    .then(json => renderTasks(json))
+    // pass the whole array to the function renderTasks to iterate each task
     .catch(error => console.log("Error!!!"))
+}
+
+const renderTasks = (tasks) => {
+    tasks.forEach(element => {
+        // calling forEach on tasks array 
+        // element = current element
+        const li = document.createElement("li")
+        // declaring and assigning a variable
+        // storing the created Element inside the variable
+        li.innerHTML = `
+            <h2 class="task-name">${element.name}</h2>
+        `
+    })
 }
